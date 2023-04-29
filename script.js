c9003b69db4d0b0e61762ba655ae6d1e2ca46bf2
abhin_2122003 <abhinshelly@gmail.com>
2023-04-29 18:58:54 +0530

    load

---
+++ script.js
@@ -0,0 +1,6 @@
+// window.onload = function (){
+//     const loadingPage = document.querySelector(".loading-page");
+//     setTimeout(()=>{
+//         loadingPage.style.display = "none";
+//     },2000)
+// }
