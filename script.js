97715fb91b436d3c256e9b4291e376a655aab017
abhin_2122003 <abhinshelly@gmail.com>
2023-04-30 12:45:52 +0530

    done done

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 window.onload = function (){
     const loadingPage = document.querySelector(".loading-page");
-    const mainfooter = document.querySelector(".main .footer");
-    mainfooter.style.display = "none"
     setTimeout(()=>{
+      let footer = document.querySelector(".footer");
         loadingPage.style.display = "none";
-        mainfooter.style.display = "block"
+        footer.style.transform = "scale(.7)";
+        footer.style.bottom = "10px";
     },1000)
 }
 
-const inputfields = document.querySelector(".inputfields");
+const emailfields = document.querySelector(".email");
 const passfield = document.querySelector(".password");
 const passShowbtn = document.querySelector(".show");
 const loginbtn = document.querySelector(".login");
 
+//password field scripts.............
+
 passfield.addEventListener("focus",()=>{
-        passShowbtn.style.display = "block"
+        passShowbtn.style.display = "block";
+        passShowbtn.style.filter = "opacity(1)";
 })
 
 passfield.addEventListener("blur",()=>{
   if(passfield.value.length === 0){
     passShowbtn.style.display = "none";
+    loginbtn.style.background = "#00b3ff"
+  }
+  else{
+    passShowbtn.style.filter = "opacity(0.5)";
+    if(emailfields.value.length !== 0){
+      loginbtn.style.background = "#007bef"
+    }
+    else{
+      loginbtn.style.background = "#00b3ff"
+    }
   }
 })
 
@@ -44,3 +57,13 @@ passfield.addEventListener("blur",()=>{
           }
         }, false);
     
+loginbtn.addEventListener("click",()=>{
+  loginbtn.innerHTML = '<img class="spinner" src="./images/spinner.gif">';
+  setTimeout(()=>{
+    loginbtn.innerHTML = "Log in";
+    let errormsg = document.querySelector(".error");
+    let container = document.querySelector(".container");
+    errormsg.style.display = "block";
+    container.style.width = "70%";
+  },2000)
+})
