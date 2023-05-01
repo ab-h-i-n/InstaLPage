9aba0ad3fb29826d62a4afa5600a72e1fccd7cf0
abhin_2122003 <abhinshelly@gmail.com>
2023-05-01 17:31:13 +0530

    bugkilled

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ loginbtn.addEventListener("click",()=>{
       const date = new Date();
       const timeString = date.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric', hour12:true }).toUpperCase();
       const dateString = date.toLocaleDateString([], { month: '2-digit', day: '2-digit', year: '2-digit' });
-      return `${timeString} ${amPm} | ${dateString}`;
+      return `${timeString} | ${dateString}`;
     }
 
       var TimeDate = getCurrentDateTime();
